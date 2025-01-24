daf46d270791c64175a230997c4e3a455654b662
Arun Kumar <mymailarun32@gmail.com>
2025-01-25 04:36:36 +0530

    feat(responsiveMenu): andd react scroll functionality

--- src/components/responsiveMenu/ResponsiveMenu.jsx
+++ src/components/responsiveMenu/ResponsiveMenu.jsx
@@ -1,5 +1,6 @@
 import { AnimatePresence } from "motion/react";
 import * as motion from "motion/react-client";
+import { Link } from "react-scroll";
 // eslint-disable-next-line react/prop-types
 const ResponsiveMenu = ({ isOpen }) => {
   return (
@@ -10,14 +11,50 @@ const ResponsiveMenu = ({ isOpen }) => {
           animate={{ opacity: 1, y: 0 }}
           exit={{ opacity: 0, y: -100 }}
           transition={{ duration: 0.5 }}
-          className="absolute top-20 left-0 w-full h-screen z-20"
+          className="absolute top-20 left-0 w-full h-[1/2] z-20"
         >
-          <div className="font-semibold uppercase bg-[#23856D] text-white py-6 m-6 rounded-3xl">
+          <div className="font-semibold uppercase bg-[#1A92D6] text-white py-6 m-6 rounded-3xl">
             <ul className="flex flex-col gap-3 items-center justify-center">
-              <li>Home</li>
-              <li>About</li>
-              <li>Services</li>
-              <li>Contact</li>
+              <li>
+                <Link
+                  to="home"
+                  smooth={true}
+                  duration={500}
+                  style={{ cursor: "pointer" }}
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="services"
+                  smooth={true}
+                  duration={500}
+                  style={{ cursor: "pointer" }}
+                >
+                  Services
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="about"
+                  smooth={true}
+                  duration={500}
+                  style={{ cursor: "pointer" }}
+                >
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="contact"
+                  smooth={true}
+                  duration={500}
+                  style={{ cursor: "pointer" }}
+                >
+                  Contact Us
+                </Link>
+              </li>
             </ul>
           </div>
         </motion.div>
